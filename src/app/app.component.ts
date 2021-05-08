ad5fe0633c3c89cf659401f4388d62e6163e6f10
Anuradha Mudalige <anuradamudalige@gmail.com>
2021-05-08 14:36:43 +0530

    Initial project sharing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Template} from './template';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(3000)),
+    ]),
+  ]
 })
-export class AppComponent {
-  title = 'slicing';
+export class AppComponent implements OnInit {
+  templateList: Template[] = [];
+  selectedTemplateId: number | undefined;
+
+  ngOnInit(): void {
+    this.templateList.push({id: 1});
+    this.templateList.push({id: 2});
+    this.selectedTemplateId = 1;
+  }
+
+  onSelect(template: Template): void {
+    this.selectedTemplateId = template.id;
+  }
 }
