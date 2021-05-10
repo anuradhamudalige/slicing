fae236bfe5d591cd9b5c676f38da5b0d9ff14704
Anuradha Mudalige <anuradamudalige@gmail.com>
2021-05-10 12:23:27 +0530

    Carousel added

--- src/app/modules/shared/components/carousel/carousel.component.ts
+++ src/app/modules/shared/components/carousel/carousel.component.ts
@@ -1,41 +1,67 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { CarouselItem } from '../../models/carousel-item';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss'],
-  animations: [
-    trigger('prevNext', [
-      state('prev', style({transform: 'translateX(100px) ease-in'})),
-      state('next', style({transform: 'translateX(-100px)'})),
-      transition('prev <=> next', animate(1000)),
-      transition('* <=> *', animate(1000))
-    ])
-  ]
+  styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements AfterViewInit {
 
+  @ViewChild('wrapper') wrapperEl: ElementRef | undefined;
   @Input()
   carouselItemList: CarouselItem[] = [];
   currentTransition = '';
+  amount = 0;
+  maxOffset = 0;
+  maxOffsetRight = 0;
+  maxOffsetLeft = 0;
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initializeOffset();
   }
 
-  prev(): void {
-    this.currentTransition = 'prev';
+  prev(event: any, wrapperEl: any): void {
+    this.calculateMaxOffset(wrapperEl.offsetWidth);
+    let movingOffset = 250;
+    if (this.maxOffsetLeft < 250) {
+      movingOffset = this.maxOffsetLeft;
+    }
+    this.amount += movingOffset;
+    this.maxOffsetRight += movingOffset;
+    this.maxOffsetLeft -= movingOffset;
   }
 
-  next(): void {
-    this.currentTransition = 'next';
+  next(event: any, wrapperEl: any): void {
+    this.calculateMaxOffset(wrapperEl.offsetWidth);
+    let movingOffset = 250;
+    if (this.maxOffsetRight < 250) {
+      movingOffset = this.maxOffsetRight;
+    }
+    this.amount -= movingOffset;
+    this.maxOffsetLeft += movingOffset;
+    this.maxOffsetRight -= movingOffset;
   }
 
-  // onAnimationEvent(): void {
-  //   this.currentTransition = '';
-  // }
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.initializeOffset();
+  }
+
+  initializeOffset(): void {
+    setTimeout(() => {
+      this.calculateMaxOffset(this.wrapperEl?.nativeElement.offsetWidth);
+      this.maxOffsetRight = this.maxOffset < 0 ? 0 : this.maxOffset;
+      this.maxOffsetLeft = 0;
+      this.amount = 0;
+    });
+  }
+
+  calculateMaxOffset(windowWidth: number): void {
+    const itemCount = this.carouselItemList.length;
+    this.maxOffset = Math.abs(windowWidth - (itemCount * 250 + (10 * (itemCount - 1))));
+  }
 }
